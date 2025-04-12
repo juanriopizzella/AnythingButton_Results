75c710f70c91e21cc4eddf2d4a34f36a9d8d4f98
Gareth Volka <GarethVolka@hawkinsbrown.com>
2025-04-12 21:32:56 +0100

    update

--- AnythingButton_ResultsComponent.cs
+++ AnythingButton_ResultsComponent.cs
@@ -7,30 +7,4 @@ using System.Collections.Generic;
 namespace AnythingButton_Results
 {
     
-    public class AnythingButton_ResultsComponent : GH_Component
-    {
-
-        public AnythingButton_ResultsComponent()
-          : base("AnythingButton_ResultsComponent", "Nickname",
-            "Description",
-            "Category", "Subcategory")
-        {
-        }
-
-        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
-        {
-        }
-
-        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
-        {
-        }
-
-        protected override void SolveInstance(IGH_DataAccess DA)
-        {
-        }
-
-        protected override System.Drawing.Bitmap Icon => null;
-
-        public override Guid ComponentGuid => new Guid("f5a32c0e-d422-4103-99d0-e18953ee0d25");
-    }
 }
